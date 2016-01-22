1fb06963d05c8f9588f2642f63df28b02e271e10
Raphael De Freitas <raphael@de-freitas.net>
2016-01-22 11:22:30 +0100

    bootstrapping the project

---
+++ MajoraOAuthServerBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Majora\OAuthServerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MajoraOAuthServerBundle
+ *
+ * @author Raphael De Freitas <raphael@de-freitas.net>
+ */
+class MajoraOAuthServerBundle extends Bundle
+{
+
+}
